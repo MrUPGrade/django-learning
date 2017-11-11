38f54014db14ad0996b93ae1940091595bff05b8
Jakub Czapliński <sirupgrade@gmail.com>
2017-11-11 21:00:06 +0100

    cleanup in code

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -2,19 +2,22 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from rest_framework import urls as  drf_urls
+
 from basics.views.rapi import router
+from basics import urls as basics_urls
 
 from rest_framework.schemas import get_schema_view
 
 schema_view = get_schema_view(title='Pastebin API')
 
-
 urlpatterns = [
-    url(r'^', include('basics.urls')),
+    url(r'^', include(basics_urls)),
+
     url(r'^admin/', admin.site.urls),
 
     url(r'^rapi/', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-auth/', include(drf_urls)),
     url(r'^schema/$', schema_view),
 ]
 
